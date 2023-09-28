0557a7b47c88f880fd83e9a4c1c2011cb57fa153
arifur946954 <arif.bd08060@gmail.com>
2023-09-28 23:57:03 +0600

    properties to controller

--- src/main/java/com/first/demo/Controller/HomeController.java
+++ src/main/java/com/first/demo/Controller/HomeController.java
@@ -1,10 +1,23 @@
 package com.first.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @Value("${coach.name}")
+    private  String CoachName;
+
+    @Value("${team.name}")
+    private  String TeamName;
+    @GetMapping("/teamInfo")
+    public String TeamInfo(){
+        return "Coach name is: "+CoachName +" "+ "Team name is: "+TeamName;
+
+    }
+
 @GetMapping("/")
     public  String sayHello(){
         return  "Hello world Arifur gf Rahman";
